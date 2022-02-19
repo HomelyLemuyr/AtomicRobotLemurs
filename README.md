d444a90d0c7e0ad0f3809f1922d3d4be94ddc9db
Wilbert A Martinez Jr <wamj45@gmail.com>
2022-02-18 23:37:57 -0600

    Added the regular expressions for bin and hex inputs

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 To run in terminal (UNIX machine):
 ```python3 app.py```
+
+
+#### Note
+All Hex numbers MUST be prefixed with ```0x``` such as
+```
+0x19AC 0x8A12 0xFFFFFFF
+```
--- io_manager.py
+++ io_manager.py
@@ -1,6 +1,27 @@
 # Handle the input of numbers
 import re
 
+# Regular Expressions for inputs
+HEX_RE = "0x[0-9,A-F,a-f]+$"
+BIN_RE = "^[0-1]+$"
+
+# Make this list into a dict like this:
+"""
+ {
+    inputs: [
+        "input1": {
+            "type": "hex",
+            "value": "0x19AC"
+        },
+        "input2": {
+            "type": "hex",
+            "value": "0xFFFF"
+        }
+    ]
+}
+"""
+
+# create the input dict and set each inputs value in the dict
 def get_inputs():
     nums = []
     inputs = input("Please enter your digits: ")
@@ -9,6 +30,12 @@ def get_inputs():
             nums.append(i)
     return nums
 
-def classify_input(input):
-    
-    return type
+# take in the dict then set the type for each input
+def classify_input(input, inputs):
+    for input in inputs:
+        if re.search(HEX_RE, input) is True:
+            # set inputx.type = hex
+            pass
+        elif re.search(BIN_RE, input) is True:
+            # set inputx.type = bin
+    return inputs
