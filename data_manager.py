dccb37d6caa463301059e7997946cb81e7d0441c
Wilbert A Martinez Jr <wamj45@gmail.com>
2022-02-18 23:54:22 -0600

    Added xor and multiplication methods

--- data_manager.py
+++ data_manager.py
@@ -14,3 +14,19 @@ class DataManager:
     def check_input(self):
         for input in self.inputs:
             io_manager.classify_input(input)
+
+
+    # It might be easier to convert the inputs to dec. Preform math operations. Then convert back to different formats
+    def convert_to_dec(self):
+        inputs = self.inputs["inputs"]
+        for input in inputs:
+            if input["type"] == "hex":
+        pass
+
+    def xor(self, val1, val2):
+        ret = val1 ^ val2
+        return ret
+
+    def multiply(self, val1, val2):
+        mult = val1 * val2
+        return mult
